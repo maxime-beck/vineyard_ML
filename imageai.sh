e354fe059a013b6a44949a3c86511585eb3c98a9
Maxime Beck <mbcompte@gmail.com>
2019-03-27 23:48:15 +0100

    Initial commit

---
+++ imageai.sh
@@ -0,0 +1,11 @@
+#/bin/bash!
+
+./python -m pip install --upgrade tensorflow
+./python -m pip install numpy
+./python -m pip install scipy
+./python -m pip install opencv-python
+./python -m pip install pillow
+./python -m pip install matplotlib
+./python -m pip install h5py
+./python -m pip install keras
+./python -m pip install https://github.com/OlafenwaMoses/ImageAI/releases/download/2.0.2/imageai-2.0.2-py3-none-any.whl
